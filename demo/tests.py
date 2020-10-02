10fc814f807bccc449e5f28c232c48d13ca2abe6
Shubhashri AG <agshubhashri@gmail.com>
2020-10-02 23:54:06 +0530

    test function changed

--- demo/tests.py
+++ demo/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 
 
 class DemoTestCase(TestCase):
-    def test_main_page(self):
+
+    def load_main_page(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
